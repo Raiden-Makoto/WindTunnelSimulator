decff697eeafc6680d3c42bf21e1fd9de7cd004a
Max <mc.cui@mail.utoronto.ca>
2025-12-27 09:32:31 -0800

    added boundary conditions

--- sphere_flow.py
+++ sphere_flow.py
@@ -57,3 +57,19 @@ def boundary_sphere(x, on_boundary):
         np.isclose(x[0], 0) or np.isclose(x[0], 2) or
         np.isclose(x[1], 0) or np.isclose(x[1], 1)
     )
+
+# Rule 1: Inlet Flow (Parabolic profile, fastest in middle)
+bc_inlet_u = dde.icbc.DirichletBC(geom, lambda x: 4 * u_inlet * x[:, 1:2] * (1 - x[:, 1:2]), boundary_inlet, component=0)
+bc_inlet_v = dde.icbc.DirichletBC(geom, lambda x: 0, boundary_inlet, component=1)
+
+# Rule 2: No-Slip Condition (Air sticks to walls and sphere, velocity=0)
+bc_walls_u = dde.icbc.DirichletBC(geom, lambda x: 0, boundary_walls, component=0)
+bc_walls_v = dde.icbc.DirichletBC(geom, lambda x: 0, boundary_walls, component=1)
+bc_sphere_u = dde.icbc.DirichletBC(geom, lambda x: 0, boundary_sphere, component=0)
+bc_sphere_v = dde.icbc.DirichletBC(geom, lambda x: 0, boundary_sphere, component=1)
+
+# Rule 3: Outlet (Pressure is zero, air leaves freely)
+bc_outlet_p = dde.icbc.DirichletBC(geom, lambda x: 0, boundary_outlet, component=2)
+
+# Combine all Boundary Conditions
+bcs = [bc_inlet_u, bc_inlet_v, bc_walls_u, bc_walls_v, bc_sphere_u, bc_sphere_v, bc_outlet_p]
